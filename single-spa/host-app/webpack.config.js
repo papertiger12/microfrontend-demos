c8f6b94cc2c8856a97a9ef35e624484a5e4fc9a2
Ross Myers <ross.myers@goodrx.com>
2022-06-15 10:45:21 -0600

    single-spa loading

--- single-spa/host-app/webpack.config.js
+++ single-spa/host-app/webpack.config.js
@@ -1,15 +1,28 @@
 const { merge } = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa-react");
+const singleSpaDefaults = require("webpack-config-single-spa");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "goodrx";
   const defaultConfig = singleSpaDefaults({
-    orgName: "goodrx",
-    projectName: "host-app",
+    orgName,
+    projectName: "root-config",
     webpackConfigEnv,
     argv,
+    disableHtmlGeneration: true,
   });
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+      }),
+    ],
   });
 };
